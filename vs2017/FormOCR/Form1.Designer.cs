4b70d82d2b513af1e5eb664720ad94e617b952b6
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2024-06-20 06:15:29 +0900

    Add files via upload

---
+++ vs2017/FormOCR/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FormOCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_OpenFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_OpenFile
+            // 
+            this.button_OpenFile.Location = new System.Drawing.Point(12, 12);
+            this.button_OpenFile.Name = "button_OpenFile";
+            this.button_OpenFile.Size = new System.Drawing.Size(87, 31);
+            this.button_OpenFile.TabIndex = 0;
+            this.button_OpenFile.Text = "OpenFile";
+            this.button_OpenFile.UseVisualStyleBackColor = true;
+            this.button_OpenFile.Click += new System.EventHandler(this.button_OpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 1041);
+            this.Controls.Add(this.button_OpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_OpenFile;
+    }
+}
+
---
+++ vs2017/FormOCR/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+
+namespace FormOCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            Mat img = Cv2.ImRead(ofd.FileName);
+            Mat gray = new Mat();
+            Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
+
+            Mat edges = new Mat();
+            Cv2.Canny(gray, edges, 1, 100, 3);
+            Cv2.ImWrite("edges.png", edges);
+
+            Mat kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(3, 3));
+            Cv2.Dilate(edges, edges, kernel);
+
+            Point[][] contours;
+            HierarchyIndex[] hierarchy;
+            Cv2.FindContours(edges, out contours, out hierarchy, RetrievalModes.Tree, ContourApproximationModes.ApproxSimple);
+
+            var rects = new List<Point[]>();
+            for (int i = 0; i < contours.Length; i++)
+            {
+                if (Cv2.ContourArea(contours[i]) < 3000)
+                    continue;
+
+                if (hierarchy[i].Next == -1)
+                    continue;
+
+                RotatedRect rect = Cv2.MinAreaRect(contours[i]);
+                Point[] rectPoints = Cv2.BoxPoints(rect).Select(p => new Point((int)p.X, (int)p.Y)).ToArray();
+                rects.Add(rectPoints);
+            }
+
+            rects = rects.OrderBy(r => r[0].Y).ThenBy(r => r[0].X).ToList();
+
+            Mat imgColor = new Mat();
+            Cv2.CvtColor(img, imgColor, ColorConversionCodes.BGRA2BGR);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < rects.Count; i++)
+            {
+                int B = (int)(rnd.NextDouble() * 255.0);
+                int G = (int)(rnd.NextDouble() * 255.0);
+                int R = (int)(rnd.NextDouble() * 255.0);
+
+                Scalar color = new Scalar(B, G, R);
+                Cv2.DrawContours(imgColor, rects, i, color, 2);
+                Cv2.PutText(imgColor, i.ToString(), rects[i][0], HersheyFonts.HersheySimplex, 0.8, color, 3);
+
+                Console.WriteLine("rect:\n" + rects[i]);
+            }
+
+            string outPath = AppendIndexToFileName(ofd.FileName);
+            Cv2.ImWrite(outPath, imgColor);
+        }
+
+        public string AppendIndexToFileName(string filePath)
+        {
+            string directory = Path.GetDirectoryName(filePath);
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            string extension = Path.GetExtension(filePath);
+            int index = 1;
+
+            while (File.Exists(Path.Combine(directory, $"{fileName}{index}{extension}")))
+            {
+                index++;
+            }
+
+            return Path.Combine(directory, $"{fileName}{index}{extension}");
+        }
+
+        string langPath;
+        string lngStr = "jpn";
+
+        public string RunOCR(System.Drawing.Bitmap img)
+        {
+            Tesseract.BitmapToPixConverter cvt = new Tesseract.BitmapToPixConverter();
+            var pix = cvt.Convert(img);
+
+            using(var tesseract = new Tesseract.TesseractEngine(langPath, lngStr))
+            {
+                Tesseract.Page page = tesseract.Process(pix);
+                return page.GetText();
+            }
+        }
+    }
+}
